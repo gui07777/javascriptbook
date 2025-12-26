c654d70cb36c8d0a16f40704ebb37b81b39e6510
Guilherme Monteiro Cardoso <guilherme2006.gmc@gmail.com>
2025-12-26 18:12:12 -0300

    att

---
+++ capitulo3/exercicios/ex8/ex3_8.js
@@ -0,0 +1,28 @@
+/*
+entrada: ler velocidade permitida e ler velocidade do condutor
+processamento:
+1) se velocidade for inferior ou igual a permitida sem multa
+2) se for até 20% maior que a permitida multa leve
+3) se for superior a 20%, multa grave
+saída: exibir mensagens
+*/
+
+function calcularMulta() {
+    var inVelocidadePermitida = document.getElementById('inVelocidadePermitida').value;
+    var velocidadePermitida = Number(inVelocidadePermitida);
+    var inVelocidadeCondutor = document.getElementById('inVelocidadeCondutor').value;
+    var velocidadeCondutor = Number(inVelocidadeCondutor);
+    var outResposta = document.getElementById('outResposta');
+
+    if (velocidadeCondutor <= (velocidadePermitida * (20 / 100))) {
+        outResposta.textContent = 'Sem multa'
+    } else if (velocidadeCondutor > (velocidadePermitida * (20 / 100))) {
+        outResposta.textContent = 'Multa grave';
+    } else {
+        outResposta.textContent = 'Multa leve';
+    }
+}
+
+document.getElementById('btnVerify').addEventListener('click', calcularMulta);
+
+//incompleto, pag 106
