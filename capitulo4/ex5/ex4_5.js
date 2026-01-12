76171a3d289b519286df64b8edab313078e6904a
Guilherme Monteiro Cardoso <guilherme2006.gmc@gmail.com>
2026-01-12 17:00:00 -0300

    att

--- capitulo4/ex5/ex4_5.js
+++ capitulo4/ex5/ex4_5.js
@@ -1,23 +1,24 @@
+var numContas = 0;
+var valTotal = 0;
+var resposta = '';
+
 function processar() {
-    console.log('funcionando')
     var descricao = document.getElementById('inDescricao').value;
     var inValor = document.getElementById('inValor').value;
     var valor = Number(inValor);
     var outListaContas = document.getElementById('outListaContas');
     var outTotal = document.getElementById('outTotal');
-    var count = 0;
 
     if (descricao === '' || valor == 0 || isNaN(inValor)) {
         alert('Preencha corretamente os campos')
         return;
     }
 
-    do {
-        outListaContas.textContent = `${descricao} - R$: ${valor.toFixed(2)}`;
-        count++;
-        outTotal.textContent = `${count} Conta(s) - Total:`;
-        break;
-    } while (descricao && valor)
+    resposta += `${descricao} - R$: ${valor.toFixed(2)} \n ------------------------------- \n`
+    numContas++;
+    valTotal += valor;
+    resposta += `${numContas} Conta(s) - Total: ${valTotal}`
+    outTotal.textContent = resposta;
 }
 
 document.getElementById('btRegistrar').addEventListener('click', processar)
