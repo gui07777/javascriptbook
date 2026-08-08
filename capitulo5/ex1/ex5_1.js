f35821cfe5b5d3d3e7570038d337cee483ad08e2
Guilherme Monteiro Cardoso <guilherme2006.gmc@gmail.com>
2026-08-08 10:26:05 -0300

    update

--- capitulo5/ex1/ex5_1.js
+++ capitulo5/ex1/ex5_1.js
@@ -5,16 +5,17 @@ var outAtendimento = document.querySelector('#outAtendimento');
 function adicionar() {
     var paciente = document.querySelector('.inPaciente').value;
     pacientes.push(paciente);
-    var lista = '';
+    var nomes = '';
     for (let i = 0; i < pacientes.length; i++) {
-        lista += `${i + 1}. ${pacientes[i]}`;
+        nomes += `${i + 1}. ${pacientes[i]}`;
     }
-    outLista.innerHTML = lista + '<br>';
+    outLista.innerHTML = `<div>${nomes}</div><br>`;
 }
 
 document.querySelector('.btnAdicionar').addEventListener('click', adicionar)
 
 function atender() {
+    if (outAtendimento.textContent != '') outAtendimento.textContent = '';
     var paciente = document.querySelector('.inPaciente').value;
     if (pacientes.includes(paciente)) {
         outAtendimento.textContent = paciente;
